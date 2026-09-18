c2e26133cca86bd0c902d5c0a4159702e22740fb
atendimentoxburguer-arch <atendimentoxburguer@gmail.com>
2026-09-18 16:00:23 -0300

    Atualizar versao principal para 4.6.0

--- CONTROLE DE CONSUMO/app.js
+++ CONTROLE DE CONSUMO/app.js
@@ -1,5 +1,5 @@
 (function () {
-    window.XBURGUER_VERSAO = "4.5.1";
+    window.XBURGUER_VERSAO = "4.6.0";
     const isLogin = /(^|\/)login\.html$/i.test(location.pathname);
 
     
@@ -39,7 +39,7 @@
     }
 
 // ========================================================
-    // X-BURGUER PWA 4.5 - sem botão visual de instalação
+    // X-BURGUER PWA 4.6 - sem botão visual de instalação
     // ========================================================
     if ("serviceWorker" in navigator) {
         window.addEventListener("load", function () {
